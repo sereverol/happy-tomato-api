308fb5ecef37d5e4efa3e59d5586414b3ea5c91c
Silfrido Reverol <silfridoer@gmail.com>
2021-06-23 12:34:11 -0400

    DB connection done

--- src/server.js
+++ src/server.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
 
+connectDB();
+
+app.get('/', (req, res) => res.send('API Running'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server initialized on port ${PORT}`));
